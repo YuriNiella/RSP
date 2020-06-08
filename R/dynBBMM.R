d477cedeabc2a62d8f42264734cad081fa5fc9fb
hugomflavio <40389021+hugomflavio@users.noreply.github.com>
2020-06-09 00:43:40 +0200

    Fix timeslot display

--- R/dynBBMM.R
+++ R/dynBBMM.R
@@ -149,7 +149,7 @@ dynBBMM <- function(input, base.raster, tags = NULL, start.time, stop.time,
     timeslots <- data.frame(
       slot = 1:(length(timebreaks) - 1),
       start = timebreaks[-length(timebreaks)],
-      stop = timebreaks[-1])
+      stop = timebreaks[-1] - 1)
 
     return(list(dbbmm = mod_dbbmm, base.raster = base.raster, valid.tracks = valid.tracks,
       group.rasters = dbbmm.rasters, timeslots = timeslots, spatial = spatial)) 
