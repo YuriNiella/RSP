932ba934b2d05264c61caa3e4af3ee93004e96a3
Yuri Niella <yuri.niella@gmail.com>
2020-08-11 11:38:24 -0300

    Import actel on tests

--- tests/testthat/test_dynBBMM_latlon_timeslot.R
+++ tests/testthat/test_dynBBMM_latlon_timeslot.R
@@ -6,20 +6,21 @@
 skip_on_travis()
 
 # Load example files
-test_that("actel inputs are working as expected", {
-	aux <- system.file(package = "RSP")[1]
-	water <<- actel::loadShape(path = aux, shape = "example_shape_geo.shp", size = 0.0001)
-	water.large <<- actel::loadShape(path = aux, shape = "example_shape_geo.shp", size = 0.0001, buffer = 0.05)
-	tl <<- actel::transitionLayer(water)
+# test_that("actel inputs are working as expected", {
+# 	aux <- system.file(package = "RSP")[1]
+# 	water <<- actel::loadShape(path = aux, shape = "example_shape_geo.shp", size = 0.0001)
+# 	water.large <<- actel::loadShape(path = aux, shape = "example_shape_geo.shp", size = 0.0001, buffer = 0.05)
+# 	tl <<- actel::transitionLayer(water)
 
-	# Subset actel results to speed up testing:
-	input <- actel::example.results
-	input$valid.detections <- input$valid.detections[c(1, 52)]
-	input$valid.detections[[1]] <- input$valid.detections[[1]][c(1:15, 60:75), ] # Select 2 track
-	input$valid.detections[[2]] <- input$valid.detections[[2]][c(1:7, 116:130), ] # Select 2 tracks (1 not valid)
+# 	# Subset actel results to speed up testing:
+# 	input <- actel::example.results
+# 	input$valid.detections <- input$valid.detections[c(1, 52)]
+# 	input$valid.detections[[1]] <- input$valid.detections[[1]][c(1:15, 60:75), ] # Select 2 track
+# 	input$valid.detections[[2]] <- input$valid.detections[[2]][c(1:7, 116:130), ] # Select 2 tracks (1 not valid)
 
-	input <<- input # export input too
-})
+# 	input <<- input # export input too
+# })
+load("actel_latlon.RData")
 
 #===============================================#
 #				TESTING STARTS					#
